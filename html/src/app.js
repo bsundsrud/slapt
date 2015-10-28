0c79766bca836d62f1bc6d3750d95b74277210bb
Benn Sundsrud <benn.sundsrud@code42.com>
2015-10-28 13:18:19 -0500

    redo frontend start

--- html/src/app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Main from './components/main';
-React.render(<Main/>,
-    document.getElementById('app')
-);
--- html/webpack.config.js
+++ html/webpack.config.js
@@ -1,38 +1,91 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
+var webpack = require('webpack');
+var CleanPlugin = require('clean-webpack-plugin');
+var HtmlPlugin = require('html-webpack-plugin');
+var ExtractPlugin = require('extract-text-webpack-plugin');
 
-var node_modules_dir = path.resolve(__dirname, 'node_modules');
+var production = process.env.NODE_ENV === 'production';
 
-var config = {
-    entry: {
-        app: ['webpack/hot/dev-server', path.resolve(__dirname, 'src/app.js')],
-        vendors: ['react', 'react/addons', 'alt', 'alt/AltContainer', 'axios', 'react-popover', 'moment', 'immutable']
-    },
-    resolve: {
-        alias: {}
-    },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-    module: {
-        noParse: [/moment.js/],
-        loaders: [
-            {test: /\.(js|jsx)$/, loaders: ['react-hot', 'babel'], exclude: [node_modules_dir]}
-        ]
-    },
-    plugins: [
-        new webpack.IgnorePlugin(/^\.\/locale$/),
-        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: path.resolve(__dirname, 'src/index.html')
+var plugins = [
+    new HtmlPlugin({
+        inject: true,
+        template: './index.html'
+    }),
+    new ExtractPlugin('bundle.css'),
+    new webpack.optimize.CommonsChunkPlugin({
+        name:      'main', // Move dependencies to our main file
+        children:  true, // Look for common dependencies in all children,
+        minChunks: 2 // How many times a dependency must come up before being extracted
+    })
+];
+
+if (production) {
+    plugins = plugins.concat([
+        new CleanPlugin('dist'),
+        // This plugin looks for similar chunks and files
+        // and merges them for better caching by the user
+        new webpack.optimize.DedupePlugin(),
+
+        // This plugins optimizes chunks and modules by
+        // how much they are used in your app
+        new webpack.optimize.OccurenceOrderPlugin(),
+
+        // This plugin prevents Webpack from creating chunks
+        // that would be too small to be worth loading separately
+        new webpack.optimize.MinChunkSizePlugin({
+            minChunkSize: 10000
+        }),
+
+        // This plugin minifies all the Javascript code of the final bundle
+        new webpack.optimize.UglifyJsPlugin(
+            {
+                mangle:   true,
+                compress: {
+                    warnings: false // Suppress uglification warnings
+                }
+            }
+        ),
+
+        // This plugins defines various variables that we can set to false
+        // in production to avoid code related to them from being compiled
+        // in our final bundle
+        new webpack.DefinePlugin({
+            __SERVER__:      !production,
+            __DEVELOPMENT__: !production,
+            __DEVTOOLS__:    !production,
+            'process.env':   {
+                BABEL_ENV: JSON.stringify(process.env.NODE_ENV),
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
         }),
         new webpack.NoErrorsPlugin()
-    ],
-    progress: true
+
+    ]);
+}
+
+module.exports = {
+    devtool: production ? false : 'eval',
+    entry: './src/index',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: plugins,
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractPlugin.extract('style', 'css')
+            },
+            {
+                test: /\.(svg|ttf|otf|eot|woff|woff2)/,
+                loader: 'file'
+            }
+        ]
+    },
 };
-
-
-module.exports = config;
